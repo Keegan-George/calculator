dec1c866d33e083d087e78b76e1d3c034485c23e
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-12-01 16:21:48 -0500

    Shorten validation unit test names for clarity and group related tests together

--- static/js/tests/validation-utils.unit.test.js
+++ static/js/tests/validation-utils.unit.test.js
@@ -1,81 +1,91 @@
 import { isValidExpression, evaluateExpression, isInvalidResult } from "../validation-utils.js";
 
-test("An empty array evaluates as an invalid expression", () => {
-    const expression = [];
-    expect(isValidExpression(expression)).toBe(false);
+
+describe("isValidExpression", () => {
+    test("Empty array is invalid", () => {
+        const expression = [];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Array of empty strings is invalid", () => {
+        const expression = ["", "", ""];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Single value is invalid", () => {
+        const expression = ["1"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Two values is invalid", () => {
+        const expression = ["1", "*"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("More than three values is invalid", () => {
+        const expression = ["1", "*", "3", "2"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("First value non-numeric is invalid", () => {
+        const expression = ["*", "*", "3"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Second value not operator is invalid", () => {
+        const expression = ["1", "2", "3"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Third value non-numeric is invalid", () => {
+        const expression = ["1", "*", "*"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Invalid operator is invalid", () => {
+        const expression = ["1", "$", "3"];
+        expect(isValidExpression(expression)).toBe(false);
+    });
+
+    test("Properly formated expression is valid", () => {
+        const expression = ["1", "+", "3"];
+        expect(isValidExpression(expression)).toBe(true);
+    });
 });
 
-test("An array with empty strings evaluates as an invalid expression", () => {
-    const expression = ["", "", ""];
-    expect(isValidExpression(expression)).toBe(false);
+describe("evaluteExpression", () => {
+    test("Evaluates valid expression", () => {
+        const expression = ["1", "+", "3"];
+        expect(evaluateExpression(expression)).toBe(4);
+    });
+
+    test("Evaluates numeric values", () => {
+        const expression = [1, "+", 3];
+        expect(evaluateExpression(expression)).toBe(4);
+    });
+
 });
 
-test("An expression with one value evaluates as invalid", () => {
-    const expression = ["1"];
-    expect(isValidExpression(expression)).toBe(false);
+describe("isInvalidResult", () => {
+    test("Numeric result is valid", () => {
+        result = 5;
+        expect(isInvalidResult(result)).toBe(false);
+    });
+
+    test("Numeric string result is valid", () => {
+        result = "5";
+        expect(isInvalidResult(result)).toBe(false);
+    });
+
+    test("Infinity is invalid", () => {
+        result = Infinity;
+        expect(isInvalidResult(result)).toBe(true);
+    });
+
+    test("NaN is invalid", () => {
+        result = NaN;
+        expect(isInvalidResult(result)).toBe(true);
+    });
 });
 
-test("An expression with two values evaluates as an invalid", () => {
-    const expression = ["1", "*"];
-    expect(isValidExpression(expression)).toBe(false);
-});
 
-test("If first value is non-numeric the expression evaluates as invalid", () => {
-    const expression = ["*", "*", "3"];
-    expect(isValidExpression(expression)).toBe(false);
-});
-
-test("If second value is not an operator the expression evaluates as invalid", () => {
-    const expression = ["1", "2", "3"];
-    expect(isValidExpression(expression)).toBe(false);
-});
-
-test("If third value is non-numeric the expression evaluates as invalid", () => {
-    const expression = ["1", "*", "*"];
-    expect(isValidExpression(expression)).toBe(false);
-});
-
-test("If expression has more than three values it evaluates as invalid", () => {
-    const expression = ["1", "*", "3", "2"];
-    expect(isValidExpression(expression)).toBe(false);
-});
-
-test("If operator is not from the list of valid operators the expression evaluates as an invalid", () => {
-    const expression = ["1", "$", "3"];
-    expect(isValidExpression(expression)).toBe(false);
-});
-
-test("A properly formated expression evaluates as valid", () => {
-    const expression = ["1", "+", "3"];
-    expect(isValidExpression(expression)).toBe(true);
-});
-
-test("Can evaluate an expression", () => {
-    const expression = ["1", "+", "3"];
-    expect(evaluateExpression(expression)).toBe(4);
-});
-
-test("Can evaluate an expression with numeric values", () => {
-    const expression = [1, "+", 3];
-    expect(evaluateExpression(expression)).toBe(4);
-});
-
-test("A numeric result is not invalid", () => {
-    result = 5;
-    expect(isInvalidResult(result)).toBe(false);
-});
-
-test("A numeric string result is not invalid", () => {
-    result = "5";
-    expect(isInvalidResult(result)).toBe(false);
-});
-
-test("Infinity is an invalid result", () => {
-    result = Infinity;
-    expect(isInvalidResult(result)).toBe(true);
-});
-
-test("NaN is an invalid result", () => {
-    result = NaN;
-    expect(isInvalidResult(result)).toBe(true);
-});
