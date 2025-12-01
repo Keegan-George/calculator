aa6678600c072a780445a61875dc8e0ac705f912
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-12-01 15:03:05 -0500

    Add additional test to validate a properly formatted expression

--- static/js/validation-utils.unit.test.js
+++ static/js/validation-utils.unit.test.js
@@ -23,24 +23,29 @@ test("An expression with two values evaluates as an invalid", () => {
 test("If first value is non-numeric the expression evaluates as invalid", () => {
     const expression = ["*", "*", "3"];
     expect(isValidExpression(expression)).toBe(false);
-})
+});
 
 test("If second value is not an operator the expression evaluates as invalid", () => {
     const expression = ["1", "2", "3"];
     expect(isValidExpression(expression)).toBe(false);
-})
+});
 
 test("If third value is non-numeric the expression evaluates as invalid", () => {
     const expression = ["1", "*", "*"];
     expect(isValidExpression(expression)).toBe(false);
-})
+});
 
 test("If expression has more than three values it evaluates as invalid", () => {
     const expression = ["1", "*", "3", "2"];
     expect(isValidExpression(expression)).toBe(false);
-})
+});
 
 test("If operator is not from the list of valid operators the expression evaluates as an invalid", () => {
     const expression = ["1", "$", "3"];
     expect(isValidExpression(expression)).toBe(false);
-})
+});
+
+test("A properly formated expression evaluates as valid", () => {
+    const expression = ["1", "+", "3"];
+    expect(isValidExpression(expression)).toBe(true);
+});
